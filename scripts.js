47ace246510c59b0485aa63020e886d0575e1e6a
willowcheng <e.wingus@Gmail.com>
2015-03-13 19:53:39 -0400

    Introduction to Javascript

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 /**
  * Created by willowcheng on 3/13/2015.
  */
+
+console.log("Begin program");
+alert("Help me fix this program!");
+alert("Can you get this message to appear?");
+document.write("<h2>My first JavaScript program</h2>");
+document.write("<p>I'm practicing 'debugging'.</p>");
+console.log("End program");
