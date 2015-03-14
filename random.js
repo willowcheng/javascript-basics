bdeb55d4299900c8c4d5e6cebaf23eb6e582fc7d
willowcheng <e.wingus@Gmail.com>
2015-03-14 14:14:34 -0400

    Working with numbers

---
+++ random.js
@@ -0,0 +1,11 @@
+/**
+ * Created by willowcheng on 3/13/2015.
+ */
+
+var input1 = prompt("Please type a starting number");
+var bottomNumber = parseInt(input1);
+var input = prompt("Please type a number");
+var topNumber = parseInt(input);
+var randomNumber = Math.floor(Math.random() * (topNumber - bottomNumber + 1)) + bottomNumber;
+var message = "<p>" + randomNumber + " is a number between " + bottomNumber + " and" + topNumber + ".</p>";
+document.write(message);
